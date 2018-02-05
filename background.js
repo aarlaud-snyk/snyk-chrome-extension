ab74fa818023f667f8cc8b3dc3d6885826706145
Antoine Arlaud <aarlaud@hotmail.com>
2018-02-05 10:44:26 -0500

    feat:Adding custom snyk instance url for on premise setup

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-
-//https://snyk.io/test/github/
-//https://snyk.io/test/npm/
+var snykurl = "snyk.io";
 
 function createNotification(url, options, createdCallback) { // callback is optional
   var opt = options;
@@ -44,90 +42,72 @@ function showSafeNotification(packageName){
 
 }
 
+
+
   chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    if(request.source == "getsnykurl"){
+      sendResponse({url: snykurl});
+    } else if(request.source == "snykurl"){
+      snykurl = request.url;
 
-    var snykTestUrl = "https://snyk.io/test/"+request.source+'/'+ request.packageName+"/"+ request.packageVersion;
-      //request.packageName
-      var xhr = new XMLHttpRequest();
-
-      xhr.open("GET", snykTestUrl +"/badge.svg", false);
-      xhr.send();
-      if(xhr.status != 200){
+      var connectionTimeout = setTimeout(function(){
+        sendResponse({status: "fail"});
+        snykurl = "snyk.io";
         return;
-      }
-      var result = xhr.responseText;
-      var el = document.createElement( 'html' );
-      el.innerHTML = result;
-      // If invalid package name
-      result = el.getElementsByClassName('header__title');
-      var errorString = "";
-      if (request.source == "npm") errorString = "Invalid npm package";
+      }, 3000);
 
-      if(result.length > 0 && result[0].textContent == errorString){
-        return;
-      }
-      // If valid package, parsing svg to extract # of issues.
-      result = el.getElementsByTagName( 'text' );
-      var nbOfVuln = result[result.length-1].textContent;
-      if(parseInt(nbOfVuln) > 0){
-          showVulnNotification(request.source, request.packageName, request.packageVersion, nbOfVuln, snykTestUrl);
-      } else {
-          showSafeNotification(request.packageName)
-      }
-      sendResponse({nbOfVuln: nbOfVuln, url: snykTestUrl});
+      fetch("https://"+snykurl +"/")
+      .then(
+        function(response) {
+          if (response.status !== 200) {
+            sendResponse({status: "fail"});
+            snykurl = "snyk.io";
+            return;
+          } else {
+            // console.log("success");
+            clearTimeout(connectionTimeout);
+            snykurl = snykurl;
+            sendResponse({status: "success"});
 
+            return;
+          }
+        }
+      )
+      .catch(function(err) {
+        console.log(err);
+        //sendResponse({status: "fail"});
+      });
+      return true;
+
+    } else {
+      var snykTestUrl = "https://"+ snykurl +"/test/"+request.source+'/'+ request.packageName+"/"+ request.packageVersion;
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("GET", snykTestUrl +"/badge.svg", false);
+        xhr.send();
+        if(xhr.status != 200){
+          return;
+        }
+        var result = xhr.responseText;
+        var el = document.createElement( 'html' );
+        el.innerHTML = result;
+        // If invalid package name
+        result = el.getElementsByClassName('header__title');
+        var errorString = "";
+        if (request.source == "npm") errorString = "Invalid npm package";
+
+        if(result.length > 0 && result[0].textContent == errorString){
+          return;
+        }
+        // If valid package, parsing svg to extract # of issues.
+        result = el.getElementsByTagName( 'text' );
+        var nbOfVuln = result[result.length-1].textContent;
+        if(parseInt(nbOfVuln) > 0){
+            showVulnNotification(request.source, request.packageName, request.packageVersion, nbOfVuln, snykTestUrl);
+        } else {
+            showSafeNotification(request.packageName)
+        }
+        sendResponse({nbOfVuln: nbOfVuln, url: snykTestUrl});
+    }
   });
-  //
-  // function getCurrentTabUrl(callback) {
-  //   // Query filter to be passed to chrome.tabs.query - see
-  //   // https://developer.chrome.com/extensions/tabs#method-query
-  //   var queryInfo = {
-  //     active: true,
-  //     currentWindow: true
-  //   };
-  //
-  //   chrome.tabs.query(queryInfo, (tabs) => {
-  //     // chrome.tabs.query invokes the callback with a list of tabs that match the
-  //     // query. When the popup is opened, there is certainly a window and at least
-  //     // one tab, so we can safely assume that |tabs| is a non-empty array.
-  //     // A window can only have one active tab at a time, so the array consists of
-  //     // exactly one tab.
-  //     var tab = tabs[0];
-  //
-  //     // A tab is a plain object that provides information about the tab.
-  //     // See https://developer.chrome.com/extensions/tabs#type-Tab
-  //     var url = tab.url;
-  //
-  //     // tab.url is only available if the "activeTab" permission is declared.
-  //     // If you want to see the URL of other tabs (e.g. after removing active:true
-  //     // from |queryInfo|), then the "tabs" permission is required to see their
-  //     // "url" properties.
-  //     console.assert(typeof url == 'string', 'tab.url should be a string');
-  //
-  //     callback(url);
-  //   });
-  //
-  //   // Most methods of the Chrome extension APIs are asynchronous. This means that
-  //   // you CANNOT do something like this:
-  //   //
-  //   // var url;
-  //   // chrome.tabs.query(queryInfo, (tabs) => {
-  //   //   url = tabs[0].url;
-  //   // });
-  //   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-  // }
-
-  // This extension loads the saved background color for the current tab if one
-  // exists. The user can select a new background color from the dropdown for the
-  // current page, and it will be saved as part of the extension's isolated
-  // storage. The chrome.storage API is used for this purpose. This is different
-  // from the window.localStorage API, which is synchronous and stores data bound
-  // to a document's origin. Also, using chrome.storage.sync instead of
-  // chrome.storage.local allows the extension data to be synced across multiple
-  // user devices.
-    //
-    // getCurrentTabUrl((url) => {
-    //
-    //   alert("url is =>"+url);
-    // });
--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
+// Copyright (c) 2018 Antoine Arlaud, Snyk Ltd. All rights reserved.
 
  function showNotification(){
    chrome.notifications.create({
@@ -20,61 +11,21 @@
        ],
        priority: 0});
  }
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+document.addEventListener('DOMContentLoaded', () => {
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+  chrome.runtime.sendMessage({source: "getsnykurl" }, function(response) {
+      document.getElementById('url').value = response.url;
 
-    callback(url);
   });
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
+  document.getElementById('save').addEventListener('click', () => {
+      var url = document.getElementById('url').value;
+      chrome.runtime.sendMessage({source: "snykurl", url: url }, function(response) {
+          document.getElementById('status').textContent = response.status;
 
-    var btn = document.getElementById('button');
-    btn.addEventListener('click', () => {
-      showNotification();
-      //console.log('test');
-    })
-
+      });
   });
 });
