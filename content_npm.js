587bf4d147dd5513814bde55d208dbb2d7b6c204
Antoine Arlaud <aarlaud@hotmail.com>
2018-03-22 16:39:15 +0100

    fix: Fixing extension after NPM front end changes

--- content_npm.js
+++ content_npm.js
@@ -5,9 +5,17 @@
 setTimeout(function(){
   var path = document.location.pathname;
   var packageName = path.replace("/package/","");
-  var packageVersion = document.getElementsByClassName("last-publisher")[0].parentNode.childNodes[3].childNodes[1].textContent;
+
+  var elems = document.querySelectorAll("*"),
+    res = Array.from(elems).find(v => v.textContent == 'version');
+    var packageVersion = res.nextSibling.textContent;
+  //var packageVersion = document.getElementsByClassName("last-publisher")[0].parentNode.childNodes[3].childNodes[1].textContent;
   //console.log(document.getElementsByClassName("last-publisher")[0].parentNode.childNodes[3].childNodes[1].textContent);
 
 chrome.runtime.sendMessage({source: "npm", packageName: packageName, packageVersion: packageVersion}, function(response) {
-  document.getElementsByClassName("package-name")[0].appendChild(getBadge(response.nbOfVuln, response.url, packageName, packageVersion));
+  document.getElementById("readme").parentNode.insertBefore(getBadge(response.nbOfVuln, response.url, packageName, packageVersion), document.getElementById("readme"));
 });}, 50);
+
+
+var eElement; // some E DOM instance
+var newFirstElement; //element which should be first in E
