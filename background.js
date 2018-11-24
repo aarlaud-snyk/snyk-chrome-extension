25ce12ddd46e40fad372d339f15742b59b55eab1
Luke Watts <luke@snyk.io>
2018-11-24 18:15:41 +0100

    feat: removes xhr and refactor
    Removes the XHR request which causes a deprecated message to be shown in Chrome.
    
    Refactor badge.js to return an image string which sources the svg from snyk.io rather than manually recreating locally.
    This makes us more resilent to changes upstream from snyk.io
    
    Updates wording in browser notifications to match phrasing from snyk.io

--- background.js
+++ background.js
@@ -1,113 +1,81 @@
 var snykurl = "snyk.io";
 
-function createNotification(url, options, createdCallback) { // callback is optional
-  var opt = options;
+const browser = window.msBrowser || window.browser || window.chrome;
 
-  chrome.notifications.create(opt, function(createdId) {
-    var handler = function(id) {
-      if(id == createdId) {
+function showVulnNotification(packageName, packageVersion, vulnCount){
+  var vulnerabilitiesPluralized = vulnCount > 1 ? 'vulnerabilities' : 'vulnerability';
 
-        chrome.tabs.create({url:url}, function(){
-          chrome.notifications.clear(id);
-          chrome.notifications.onClicked.removeListener(handler);
-        });
-
-      }
-    };
-    chrome.notifications.onClicked.addListener(handler);
-    if(typeof createdCallback == "function") createdCallback();
+  browser.notifications.create({
+    type:     'basic',
+    iconUrl:  'snyk-avatar-notification.png',
+    title:    packageName + '@' + packageVersion,
+    message:  `${vulnCount} ${vulnerabilitiesPluralized} found`,
+    requireInteraction: false,
+    priority: 0
   });
 }
 
-function showVulnNotification(packageManager,packageName, packageVersion, vulnCount, snykTestUrl){
-  var opt = {
-      type:     'basic',
-      iconUrl:  'snyk-avatar-notification.png',
-      title:    packageName + ' ' + packageVersion,
-      message:  'This package carries '+vulnCount+ ' known issues',
-      requireInteraction: false,
-      priority: 0};
-
-      createNotification(snykTestUrl, opt);
+function showSafeNotification(packageName, packageVersion = null){
+  browser.notifications.create({
+    type:     'basic',
+    iconUrl:  'snyk-avatar-notification.png',
+    title:    [packageName, packageVersion].filter(Boolean).join('@'),
+    message:  'No known vulnerabilities found.',
+    requireInteraction: false,
+    priority: 0
+  });
 }
 
-function showSafeNotification(packageName){
-  chrome.notifications.create({
-      type:     'basic',
-      iconUrl:  'snyk-avatar-notification.png',
-      title:    packageName,
-      message:  'is vulnerability free !',
-      requireInteraction: false,
-      priority: 0});
+browser.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+  if(request.source == "getsnykurl"){
+    sendResponse({url: snykurl});
+  } else if(request.source == "snykurl"){
+    snykurl = request.url;
 
-}
+    var connectionTimeout = setTimeout(function(){
+      sendResponse({status: "fail"});
+      snykurl = "snyk.io";
+      return;
+    }, 3000);
 
-
-
-  chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if(request.source == "getsnykurl"){
-      sendResponse({url: snykurl});
-    } else if(request.source == "snykurl"){
-      snykurl = request.url;
-
-      var connectionTimeout = setTimeout(function(){
-        sendResponse({status: "fail"});
-        snykurl = "snyk.io";
-        return;
-      }, 3000);
-
-      fetch("https://"+snykurl +"/")
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            sendResponse({status: "fail"});
-            snykurl = "snyk.io";
-            return;
-          } else {
-            // console.log("success");
-            clearTimeout(connectionTimeout);
-            snykurl = snykurl;
-            sendResponse({status: "success"});
-
-            return;
-          }
-        }
-      )
-      .catch(function(err) {
-        console.log(err);
-        //sendResponse({status: "fail"});
-      });
-      return true;
-
-    } else {
-      var snykTestUrl = "https://"+ snykurl +"/test/"+request.source+'/'+ request.packageName+"/"+ request.packageVersion;
-        var xhr = new XMLHttpRequest();
-
-        xhr.open("GET", snykTestUrl +"/badge.svg", false);
-        xhr.send();
-        if(xhr.status != 200){
+    fetch("https://"+snykurl +"/")
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          sendResponse({status: "fail"});
+          snykurl = "snyk.io";
           return;
-        }
-        var result = xhr.responseText;
-        var el = document.createElement( 'html' );
-        el.innerHTML = result;
-        // If invalid package name
-        result = el.getElementsByClassName('header__title');
-        var errorString = "";
-        if (request.source == "npm") errorString = "Invalid npm package";
-
-        if(result.length > 0 && result[0].textContent == errorString){
-          return;
-        }
-        // If valid package, parsing svg to extract # of issues.
-        result = el.getElementsByTagName( 'text' );
-        var nbOfVuln = result[result.length-1].textContent;
-        if(parseInt(nbOfVuln) > 0){
-            showVulnNotification(request.source, request.packageName, request.packageVersion, nbOfVuln, snykTestUrl);
         } else {
-            showSafeNotification(request.packageName)
+          // console.log("success");
+          clearTimeout(connectionTimeout);
+          snykurl = snykurl;
+          sendResponse({status: "success"});
+
+          return;
         }
-        sendResponse({nbOfVuln: nbOfVuln, url: snykTestUrl});
-    }
-  });
+      }
+    )
+    .catch(function(err) {
+      console.log(err);
+      //sendResponse({status: "fail"});
+    });
+    return true;
+
+  } else {
+    const badgeRequest = fetch(request.testPath + "/badge.svg");
+
+    badgeRequest
+      .then(function(response) { return response.text()})
+      .then(function(response) {
+        const parse = new DOMParser();
+        const doc = parse.parseFromString(response, "image/svg+xml");
+        const nbOfVuln = parseInt(doc.querySelectorAll('text')[3].innerHTML, 10);
+
+        if (nbOfVuln > 0) {
+          showVulnNotification(request.packageName, request.packageVersion, nbOfVuln);
+        } else if (nbOfVuln === 0) {
+          showSafeNotification(request.packageName, request.packageVersion);
+        }
+      });
+  }
+});
--- content_npm.js
+++ content_npm.js
@@ -1,21 +1,19 @@
+const packageName = document.location.pathname.split('/')[2];
+const packageVersion = document.location.pathname.split('/')[4] || 'latest';
+const testPath = `https://snyk.io/test/npm/${packageName}/${packageVersion}`;
 
+chrome.runtime.sendMessage({
+  source: "npm",
+  packageName,
+  packageVersion,
+  testPath,
+},
+function(response) {
+  const $anchor = document.createElement('a');
+  $anchor.setAttribute('href', `${testPath}`);
+  $anchor.innerHTML = getBadge(testPath);
 
-
-
-setTimeout(function(){
-  var path = document.location.pathname;
-  var packageName = path.replace("/package/","");
-
-  var elems = document.querySelectorAll("*"),
-    res = Array.from(elems).find(v => v.textContent == 'version');
-    var packageVersion = res.nextSibling.textContent;
-  //var packageVersion = document.getElementsByClassName("last-publisher")[0].parentNode.childNodes[3].childNodes[1].textContent;
-  //console.log(document.getElementsByClassName("last-publisher")[0].parentNode.childNodes[3].childNodes[1].textContent);
-
-chrome.runtime.sendMessage({source: "npm", packageName: packageName, packageVersion: packageVersion}, function(response) {
-  document.getElementById("readme").parentNode.insertBefore(getBadge(response.nbOfVuln, response.url, packageName, packageVersion), document.getElementById("readme"));
-});}, 50);
-
-
-var eElement; // some E DOM instance
-var newFirstElement; //element which should be first in E
+  document
+    .querySelector('#readme h1')
+    .after($anchor);
+});
